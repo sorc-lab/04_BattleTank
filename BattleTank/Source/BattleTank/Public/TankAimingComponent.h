243e31963d181256a7b5a3371cfd77534c8e3f0c
macfisher <macfisher01@gmail.com>
2017-12-14 17:26:36 -0700

    Replaced BP Turret_Barrel static mesh with C++ class and added UPROPERTY settings to BP

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -8,6 +8,7 @@
 
 class UTankBarrel;
 
+// Holds barrel's properties and methods
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankAimingComponent : public UActorComponent
 {
