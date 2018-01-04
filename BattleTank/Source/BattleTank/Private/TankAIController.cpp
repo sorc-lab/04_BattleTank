9ebd21c14bf4d2d36d6796fe9dff40b36aff3701
macfisher <macfisher01@gmail.com>
2018-01-04 09:38:15 -0700

    Minor commit before major refactoring

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -5,6 +5,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Engine/Engine.h"
 #include "GameFramework/Actor.h"
+// Depends on movement component via pathfinding system
 
 void ATankAIController::BeginPlay()
 {
