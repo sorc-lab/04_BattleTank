565320e74c3a22426a5d0a9ce8c54d9c55a0618b
macfisher <macfisher01@gmail.com>
2018-01-05 09:07:56 -0700

    Added sideways friction physics calculations to tank tracks

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -11,10 +11,34 @@ UTankTrack::UTankTrack()
 void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType,
 	FActorComponentTickFunction* ThisTickFunction)
 {
-	// calculate the slippage speed
-	auto SlippageSpeed = 
-	// work-out the required acceleration this frame to correct
-	// calculate and apply sideways force (F = m a)
+	/** NOTES ==================================================================== */
+	// FIND THE SLIPPAGE SPEED
+	// - The component of speed in the tank right direction
+	// - If there is no slippage, then it should be defaulted to 0
+	// - If sliding sideways, the slippage speed should be exactly the same as the
+	//	 tank.
+	// - Use cos of the angle between the velocity of the tank (whichever
+	//   direction it is actually going in) and the sideways right vector. So, if
+	//   it is going perfectly sideways, the cos between the two angles would be 1.
+	//   If it is going forward then the cos would be 0.
+	// - Use FVector::DotProduct() between velocity of tank component
+	//   (GetComponentVelocity()) and the tank right vector (GetRightVector).
+	/** ========================================================================== */
+
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	
+	// work-out the required acceleration this frame to correct (applied in the
+	// opposite direction)
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+
+	// calculate and apply sideways force (Force = Mass * Acceleration)
+	// get mass of tank via the TankRoot (need static mesh component to
+	// get the mass)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+
+	// divided by two because there are two tracks
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
 }
 
 void UTankTrack::SetThrottle(float Throttle)
